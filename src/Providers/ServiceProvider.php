70ae29d242beaaee23c1b796b8c61bda594139d4
MajdSoubh <majdsoubh53@gmail.com>
2024-09-02 13:00:02 +0300

    clean code & adapt collection with treeify changes

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace MS\Treeify\Providers;
 
-use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Support\Collection;
 use MS\Treeify\Treeify;
 
@@ -13,39 +12,6 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'self-tree');
-        // $this->loadViewsFrom(__DIR__ . '/../views', 'self-tree');
-        // // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__ . '/../routes/routes.php');
-
-        if ($this->app->runningInConsole())
-        {
-
-            // $this->publishes([
-            //     __DIR__ . '/../config/config.php' => config_path('self-tree.php'),
-            // ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/self-tree'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/self-tree'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/self-tree'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
-        }
     }
 
     /**
@@ -53,12 +19,9 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function register()
     {
-
-        Collection::macro('asTree', function ($onlyParents = null, $parentFieldName = null)
+        Collection::macro('treeify', function ($onlyParents = null, $parentFieldName = null)
         {
-
             $result = Treeify::treeify($this, $onlyParents, $parentFieldName);
-
             return $result;
         });
     }
