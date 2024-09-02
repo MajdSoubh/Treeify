02693cb7bca22f5ae2866f3c89b14c3d1af3e975
MajdSoubh <majdsoubh53@gmail.com>
2024-09-02 13:50:31 +0300

    add documentation and variables types

--- src/HasTree.php
+++ src/HasTree.php
@@ -3,48 +3,55 @@
 namespace MS\Treeify;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
 
 trait HasTree
 {
-
-
-    public function scopeTreeify(Builder $query, $onlyParents = true)
+    /**
+     * Scope a query to transform a flat list of models into a hierarchical tree structure.
+     *
+     * This scope method uses the `Treeify` class to convert a flat new Collectionion of models into
+     * a hierarchical tree structure based on parent-child relationships. It can be applied
+     * to a query either on a specific model instance (to build a tree starting from that model)
+     * or on the entire set of models.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  bool  $onlyTopParents  If true, only top-level parents will be included in the resulting tree.
+     * @return \Illuminate\Support\new Collectionion  The resulting tree structure.
+     */
+    public function scopeTreeify(Builder $query, bool $onlyTopParents = true): Collection
     {
-
+        // Determine the parent field name, defaulting to 'parent_id'
         $parentFieldName = property_exists($this, 'parentFieldName') ? $this->parentFieldName : 'parent_id';
 
+        // Determine if the current model instance is loaded with data
         $modelId = $this->exists ? $this->getKey() : false;
 
-
-        // return $query;
-
+        $data = [];
         $result = null;
 
         if ($modelId)
         {
-            $data = collect([$this]);
+            // If the model instance exists, start building the tree from this model
+            $data = new Collection([$this]);
             $parentIds = [$modelId];
+
             while (count($parentIds))
             {
-
-                $decendents = self::whereIn($parentFieldName, $parentIds)->get();
-                $data = $data->merge($decendents);
-                $parentIds = $decendents->pluck('id');
-            }
-
-            $result = Treeify::treeify($data, $onlyParents, $parentFieldName);
-
-            if (count($result) == 0)
-            {
-                $result->add($this);
+                // Fetch descendants of the current model
+                $descendants = self::whereIn($parentFieldName, $parentIds)->get();
+                $data = $data->merge($descendants);
+                $parentIds = $descendants->pluck($this->getKey())->all();
             }
         }
         else
         {
+            // If no specific model instance, build the tree for all models
             $data = $this->all();
-
-            $result =  Treeify::treeify($data, $onlyParents, $parentFieldName);
         }
+
+        // Use the Treeify class to build the tree structure
+        $result = Treeify::treeify($data, $onlyTopParents, $parentFieldName);
 
         return $result;
     }
